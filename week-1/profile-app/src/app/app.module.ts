c3b05b0dfe69bb5aabf951ca788289689f1bea69
George <george_rtaylor@yahoo.com>
2024-04-15 10:43:31 -0500

    commit

--- week-1/profile-app/src/app/app.module.ts
+++ week-1/profile-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
   ],
   imports: [
     BrowserModule,
