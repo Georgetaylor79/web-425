56e8bdb9d0c844143da91634709e2a54314f04cb
George <george_rtaylor@yahoo.com>
2024-04-16 21:34:29 -0500

    commit

--- week-2/composer-app/src/app/app-routing.module.ts
+++ week-2/composer-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent} from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ComposerListComponent } from './composer-list/composer-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ComposerListComponent
+  },
+  {
+    path: 'composer-list',
+    component: ComposerListComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
