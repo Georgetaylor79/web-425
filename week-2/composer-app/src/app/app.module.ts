89cddb31b3715d68452dc16e118b1e9a7664ff02
George <george_rtaylor@yahoo.com>
2024-04-17 00:41:13 -0500

    commit

--- week-2/composer-app/src/app/app.module.ts
+++ week-2/composer-app/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ComposerListComponent } from './composer-list/composer-list.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ComposerImageComponent } from './composer-image/composer-image.component';
+import { ComposerDetailsComponent } from './composer-details/composer-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComposerListComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    ComposerImageComponent,
+    ComposerDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ week-2/composer-app/src/app/composer-image/composer-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-composer-image',
+  templateUrl: './composer-image.component.html',
+  styleUrls: ['./composer-image.component.css']
+})
+export class ComposerImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
