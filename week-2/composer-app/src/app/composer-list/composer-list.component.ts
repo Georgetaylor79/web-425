72db40c4f164791e3d34d86a08d02e14de03c542
George <george_rtaylor@yahoo.com>
2024-04-17 21:44:45 -0500

    commit

--- week-2/composer-app/src/app/composer-list/composer-list.component.ts
+++ week-2/composer-app/src/app/composer-list/composer-list.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+export default class Composer {
+  fullName: String;
+  genre: String;
+
+   constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+   }
+}
+@Component ({
   selector: 'app-composer-list',
   templateUrl: './composer-list.component.html',
   styleUrls: ['./composer-list.component.css']
 })
 export class ComposerListComponent implements OnInit {
 
-  constructor() { }
+  composers: Array<Composer>;
+
+  constructor()  {
+    this.composers = [
+      new Composer("Johann Sebastian Bach","Classical"),
+      new Composer("Pyotr Ilyich Tchaikovsky","Classical"),
+      new Composer("Claude Debussy","Classical"),
+      new Composer("Richard Wagner","Classical"),
+      new Composer("Frederic Chopin","Classical")
+    ]
+  }
 
   ngOnInit(): void {
   }
