fc5e3f9ffe3a7c2f6db9966bbd5aff66a38578dd
George <george_rtaylor@yahoo.com>
2024-04-21 01:21:14 -0500

    commit

--- week-3/enchanced-composer-app/src/app/app.module.ts
+++ week-3/enchanced-composer-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ComposerListComponent } from './composer-list/composer-list.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ComposerDetailsComponent } from './composer-details/composer-details.component';
 
 
 
@@ -15,6 +16,7 @@ import { AboutComponent } from './about/about.component';
     ComposerListComponent,
     ContactComponent,
     AboutComponent,
+    ComposerDetailsComponent,
    
   ],
   imports: [
