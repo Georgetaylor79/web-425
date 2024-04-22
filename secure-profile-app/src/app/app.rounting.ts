0aa1a92eb665461dc607c8b2a649f1c94e6ae78f
George <george_rtaylor@yahoo.com>
2024-04-21 23:08:47 -0500

    commit

---
+++ secure-profile-app/src/app/app.rounting.ts
@@ -0,0 +1,13 @@
+import
+ { Routes } from '@angular/router';
+
+ export const AppRoutes: Routes = [
+  {
+      path: '',
+      component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+}
+ ]
