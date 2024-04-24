2f6d7487cf8eb8b204aa25c6ad10a6ea069c2ec0
George <george_rtaylor@yahoo.com>
2024-04-24 16:44:44 -0500

    commit

--- week-3/secure-profile-app/src/app/app.routing.ts
+++ week-3/secure-profile-app/src/app/app.routing.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent} from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
 
 export const AppRoutes: Routes = [
   {
       path: '',
-      component: HomeComponent
+      component: SigninComponent
   },
 {
       path: 'home',
