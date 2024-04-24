ef37815baa61cf3041c07ce00adb68d39a87a242
George <george_rtaylor@yahoo.com>
2024-04-24 16:21:18 -0500

    commit

--- week-3/secure-profile-app/src/app/app.module.ts
+++ week-3/secure-profile-app/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyImageComponent } from './my-image/my-image.component';
 import { MyDetailsComponent } from './my-details/my-details.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutes } from './app.routing';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MyImageComponent,
-    MyDetailsComponent
+    MyDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
