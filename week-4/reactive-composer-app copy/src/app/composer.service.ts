835c5987eee42127de1098e96b3635c22689977c
George <george_rtaylor@yahoo.com>
2024-04-28 18:25:04 -0500

    commit

---
+++ week-4/reactive-composer-app copy/src/app/composer.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { IComposer } from './composer.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComposerService {
+
+ 
+     composers: Array<IComposer>;
+
+   constructor() {
+    this.composers = [
+      {
+        composerId: 100, fullName: "Johann Sebastian Bach", genre: "Classical"
+      },
+
+      {
+        composerId: 101, fullName: "Pyotr Ilyich Tchaikovsky", genre: "Classical"
+      },
+
+      {
+        composerId: 102, fullName: "Claude Debussy", genre: "Classical"
+      },
+
+      {
+        composerId: 103, fullName: "Richard Wagner", genre: "Classical"
+      },
+
+      {
+        composerId: 104, fullName: "Frederic Chopin", genre: "Classical"
+      },
+
+    ]
+ }
+
+ getComposers() {
+      return this.composers;
+ }
+getComposer(composerId: number) {
+  for (let composer of this.composers) {
+       if (composer.composerId === composerId) {
+            return composer;
+       }
+  }
+}
+
+}
+
