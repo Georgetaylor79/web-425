f0c57a50b3ad9b08b07f37667fd20397b84fa0f5
George <george_rtaylor@yahoo.com>
2024-04-28 23:18:58 -0500

    commit

--- week-5/in-n-out-booksp1/src/app/app.module.ts
+++ week-5/in-n-out-booksp1/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
