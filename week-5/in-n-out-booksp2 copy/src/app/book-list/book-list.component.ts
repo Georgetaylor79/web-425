4447f614bd59e5738d496416e4bdee090417b8ab
George <george_rtaylor@yahoo.com>
2024-05-07 22:28:11 -0500

    commit

---
+++ week-5/in-n-out-booksp2 copy/src/app/book-list/book-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBook} from '../book.interface';
+import { BooksService } from '../books.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  books: Observable<IBook[]>;
+  header: Array<string> = ['isbn', 'title', 'numOfPages', 'authors'];
+  book: IBook;
+  
+  constructor(private booksService: BooksService) {
+    this.books= this.booksService.getBooks();
+   }
+
+  ngOnInit(): void {
+  }
+
+
+  showBookDetails(isbn: string) {
+    this.book = this.booksService.getBook(isbn);
+    console.log(this.book);
+  }
+
+}
+
