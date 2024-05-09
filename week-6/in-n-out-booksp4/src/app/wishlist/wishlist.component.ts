08519a525fad50430e2bd6ec3a4924717a9609b9
George <george_rtaylor@yahoo.com>
2024-05-08 22:37:33 -0500

    commit

--- week-6/in-n-out-booksp4/src/app/wishlist/wishlist.component.ts
+++ week-6/in-n-out-booksp4/src/app/wishlist/wishlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IWishListItem } from '../wishlist-item.interface';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
+  items: Array<IWishListItem> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateItemHandler(item: IWishListItem) {
+    this.items.push(item);
   }
 
 }
