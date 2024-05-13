44714c330b0d684ed971d5d671b50ca6bb17a569
George <george_rtaylor@yahoo.com>
2024-05-12 22:06:48 -0500

    commit

--- week-6/gpa-calculator-app2/src/app/grade-summary/grade-summary.component.ts
+++ week-6/gpa-calculator-app2/src/app/grade-summary/grade-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-grade-summary',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradeSummaryComponent implements OnInit {
 
-  constructor() { }
+  @Input() grade: string;
+  @Input() course: string;
+
+  constructor() {
+
+
+   }
 
   ngOnInit(): void {
   }
