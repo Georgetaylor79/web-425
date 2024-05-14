159978c02e8034b25a1ab764bb595a47466c4806
George <george_rtaylor@yahoo.com>
2024-05-13 23:03:37 -0500

    commit

--- week-7/gpa-calculator-app3/src/app/sign-in.service.ts
+++ week-7/gpa-calculator-app3/src/app/sign-in.service.ts
@@ -5,5 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class SignInService {
 
-  constructor() { }
+  studentIds: Array<number>;
+
+  constructor() {
+    this.studentIds = [1007, 1008, 1009, 1010, 1011, 1012];
+   }
+
+   validate(studentId: number) {
+    return this.studentIds.some(id => id === studentId);
+   }
 }
