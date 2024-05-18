caf355d1dff82f79d33b97e271321e89c58fd70c
George <george_rtaylor@yahoo.com>
2024-05-18 14:49:41 -0500

    commit

--- week-7/gpa-calculator-app3/src/app/sign-in/sign-in.component.ts
+++ week-7/gpa-calculator-app3/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SignInService } from '../sign-in.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  signinForm: FormGroup;
+  errorMessage: String;
+
+  constructor(private router:Router, private cookieService: CookieService, private fb: FormBuilder, private signinService: SignInService ) {
+    console.log(this.cookieService.get('session_user'));
+   }
 
   ngOnInit(): void {
+    this.signinForm = this.fb.group({
+      studentId: ''
+    })
+  }
+
+  onSubmit(){
+    const formValues = this.signinForm.value;
+    const studentId = parseInt(formValues.studentId);
+
+    if (this.signinService.validate(studentId)) {
+      this.cookieService.set('session_user', studentId.toString(),1)
+      this.router.navigate(['/'])
+    }else {
+      this.errorMessage = `The student ID you entered is invalid, please try again.`;
+    }
   }
 
 }
