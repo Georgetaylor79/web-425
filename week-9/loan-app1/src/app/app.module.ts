8895d6dc0cbf0647e69ecd90c04752f38528297a
George <george_rtaylor@yahoo.com>
2024-05-19 22:06:38 -0500

    commit

--- week-9/loan-app1/src/app/app.module.ts
+++ week-9/loan-app1/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BookListComponent } from './book-list/book-list.component';
+import { BookListComponent } from './home/book-list.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
